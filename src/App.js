0aa03d4f4b34bf42ac162e239f0725b300c871ff
penshuilong <zhao.zefe@northeastern.edu>
2023-12-04 02:00:44 -0500

    a6

--- src/App.js
+++ src/App.js
@@ -3,23 +3,25 @@ import Labs from "./Labs";
 import React from 'react';
 import HelloWorld from "./Labs/a3/HelloWorld";
 import Kanbas from "./Kanbas";
-import {HashRouter} from "react-router-dom";
-import {Routes, Route, Navigate} from "react-router";
+import { HashRouter } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router";
+import Project from "./project";
 
 
 
 
 function App() {
-  return (
-      <HashRouter>
-          <Routes>
-              <Route path="/"         element={<Navigate to="/Labs"/>}/>
-              <Route path="/hello"    element={<HelloWorld/>}/>
-              <Route path="/Labs/*" element={<Labs/>}/>
-              <Route path="/Kanbas/*" element={<Kanbas/>}/>
-          </Routes>
-      </HashRouter>
-  );
+    return (
+        <HashRouter>
+            <Routes>
+                <Route path="/" element={<Navigate to="/project" />} />
+                <Route path="/project/*" element={<Project />} />
+                <Route path="/hello" element={<HelloWorld />} />
+                <Route path="/Labs/*" element={<Labs />} />
+                <Route path="/Kanbas/*" element={<Kanbas />} />
+            </Routes>
+        </HashRouter>
+    );
 }
 
 export default App;
